c5b3dc97e2a9ca8aa9ac61917710a89e46cfaf87
Aaron <toscleveland@gmail.com>
2020-09-15 21:59:39 -0500

    “Init Project”

---
+++ CustomUserInterface/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomAnimations
+//
+//  Created by Aaron Cleveland on 9/15/20.
+//  Copyright © 2020 Aaron Cleveland. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemRed
+    }
+    
+    
+}
+
